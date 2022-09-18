085adc96c13fc08bfa3ae1a758934a7a0ffabbb3
Jonas Hiltl <jonashiltl2003@gmail.com>
2022-09-18 19:21:30 +0200

    Update Swift App

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,17 @@
 import SwiftUI
+import shared
 
 @main
 struct iOSApp: App {
-	var body: some Scene {
-		WindowGroup {
-			ContentView()
-		}
-	}
+    /**
+     you can also initialise koin using an app delegate
+     */
+    init(){
+        KoinKt.doInitKoin()
+    }
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
 }
